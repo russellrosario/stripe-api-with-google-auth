f4b56e34ec03c49a7645661cb7686b10b686fea5
Russell Rosario <rosariorussell@gmail.com>
2018-06-16 16:47:40 -0400

    initial commit

---
+++ BoilerplateComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        My component!
+        </div>
+    )
+  }
+}
+
+export default MyComponent
