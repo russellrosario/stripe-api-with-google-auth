68d64fecc97546ff9c13486a93ef7a0d4d3b8cce
Russell Rosario <rosariorussell@gmail.com>
2018-06-19 05:08:33 -0400

    updated project and next steps

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        RemotelyJS
+        </h1>
+      Find remote JavaScript jobs for developers around the world!
+        </div>
+  )
+}
+
+export default Landing
---
+++ client/src/utils/validateEmail.js
@@ -0,0 +1,14 @@
+const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim())
+    .filter(email => re.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `Email is invalid: ${invalidEmails}`;
+  }
+
+  return;
+};
